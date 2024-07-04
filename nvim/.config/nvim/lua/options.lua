3a37d572524d1e4c8db52016bc0c0b3bcbe147e4
Sey Vaneerdewegh <vaneerdewegh.sey@gmail.com>
2024-07-04 12:32:31 +0200

    feat: add vim tab config + show dotfiles in telescope

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,6 +2,10 @@
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=4'
+vim.cmd 'set softtabstop=4'
+vim.cmd 'set shiftwidth=4'
 
 -- Make line numbers default
 vim.opt.number = true
